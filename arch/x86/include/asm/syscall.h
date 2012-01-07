IzumiYouka/android_kernel_xiaomi_sm8250

commit b4ff1d7f120bd8bdb0cab6d742988ee489254d53

trace: Include <asm/asm-offsets.h> in trace_syscalls.c
Include <asm/asm-offsets.h> into trace_syscalls.c, to allow for
NR_syscalls to be automatically generated.

Cc: Steven Rostedt <rostedt@goodmis.org>
Cc: Frederic Weisbecker <fweisbec@gmail.com>
Cc: Ingo Molnar <mingo@elte.hu>
Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

kernel/trace/trace_syscalls.c
@@ -6,6 +6,7 @@
 #include <linux/ftrace.h>
 #include <linux/perf_event.h>
 #include <asm/syscall.h>
+#include <asm/asm-offsets.h>
 
 #include "trace_output.h"
 #include "trace.h"

commit 53c40d4b3df6da3ae471fcad3c1a2dc70ba4f9f7

x86: Move <asm/asm-offsets.h> from trace_syscalls.c to asm/syscall.h
This reverts commit b4ff1d7f120bd8bdb0cab6d742988ee489254d53, which
caused large numbers of build warnings on PowerPC.

This moves the #include <asm/asm-offsets.h> to <asm/syscall.h>, which
makes some kind of sense since NR_syscalls is syscalls related.

Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
Signed-off-by: H. Peter Anvin <hpa@zytor.com>
Link: http://lkml.kernel.org/r/20111214181545.6e13bc954cb7ddce9086e861@canb.auug.org.au

arch/x86/include/asm/syscall.h
@@ -15,6 +15,7 @@
 
 #include <linux/sched.h>
 #include <linux/err.h>
+#include <asm/asm-offsets.h>	/* For NR_syscalls */
 
 extern const unsigned long sys_call_table[];
 

kernel/trace/trace_syscalls.c
@@ -6,7 +6,6 @@
 #include <linux/ftrace.h>
 #include <linux/perf_event.h>
 #include <asm/syscall.h>
-#include <asm/asm-offsets.h>
 
 #include "trace_output.h"
 #include "trace.h"