IzumiYouka/android_kernel_xiaomi_sm8250

commit a2eb5fba1b0897d5573b0ba054d6f8b647e91474

staging: wilc1000: rename wilc_firmware in the struct wilc
This patch renames wilc_firmware in the struct wilc to the firmware. In
addition, null assignments to the wl->firmware after release_firmware
are removed because it is not necessary.

Signed-off-by: Tony Cho <tony.cho@atmel.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/staging/wilc1000/linux_wlan.c
@@ -537,7 +537,7 @@ int linux_wlan_get_firmware(perInterface_wlan_t *p_nic)
 		goto _fail_;
 	}
 #endif
-	g_linux_wlan->wilc_firmware = wilc_firmware;
+	g_linux_wlan->firmware = wilc_firmware;
 
 _fail_:
 
@@ -594,7 +594,7 @@ static int linux_wlan_firmware_download(struct wilc *p_nic)
 
 	int ret = 0;
 
-	if (g_linux_wlan->wilc_firmware == NULL) {
+	if (!g_linux_wlan->firmware) {
 		PRINT_ER("Firmware buffer is NULL\n");
 		ret = -ENOBUFS;
 		goto _FAIL_;
@@ -603,16 +603,15 @@ static int linux_wlan_firmware_download(struct wilc *p_nic)
 	 *      do the firmware download
 	 **/
 	PRINT_D(INIT_DBG, "Downloading Firmware ...\n");
-	ret = wilc_wlan_firmware_download(g_linux_wlan->wilc_firmware->data,
-					  g_linux_wlan->wilc_firmware->size);
+	ret = wilc_wlan_firmware_download(g_linux_wlan->firmware->data,
+					  g_linux_wlan->firmware->size);
 	if (ret < 0)
 		goto _FAIL_;
 
 	/* Freeing FW buffer */
 	PRINT_D(INIT_DBG, "Freeing FW buffer ...\n");
 	PRINT_D(INIT_DBG, "Releasing firmware\n");
-	release_firmware(g_linux_wlan->wilc_firmware);
-	g_linux_wlan->wilc_firmware = NULL;
+	release_firmware(g_linux_wlan->firmware);
 
 	PRINT_D(INIT_DBG, "Download Succeeded\n");
 
@@ -1709,8 +1708,8 @@ void wl_wlan_cleanup(void)
 			nic[i] = netdev_priv(g_linux_wlan->vif[i].ndev);
 	}
 
-	if (g_linux_wlan && g_linux_wlan->wilc_firmware)
-		release_firmware(g_linux_wlan->wilc_firmware);
+	if (g_linux_wlan && g_linux_wlan->firmware)
+		release_firmware(g_linux_wlan->firmware);
 
 	if (g_linux_wlan &&
 	   (g_linux_wlan->vif[0].ndev || g_linux_wlan->vif[1].ndev)) {

drivers/staging/wilc1000/wilc_wfi_netdevice.h
@@ -182,7 +182,7 @@ struct wilc {
 
 	unsigned char eth_src_address[NUM_CONCURRENT_IFC][6];
 
-	const struct firmware *wilc_firmware;
+	const struct firmware *firmware;
 
 #ifdef WILC_SDIO
 	struct sdio_func *wilc_sdio_func;

commit 31ced888ba292ef96cbb52f26fe4fa7f12b8554d

staging: wilc1000: fix rmmod failure
This patch fixes rmmod failure. wilc->firmware needs to be set to NULL because
it is used again to check firmware is released when module exit.

Fixes: a2eb5fba1b08 ("staging: wilc1000: rename wilc_firmware in the struct wilc")
Signed-off-by: Glen Lee <glen.lee@atmel.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/staging/wilc1000/linux_wlan.c
@@ -506,6 +506,7 @@ static int wilc1000_firmware_download(struct net_device *dev)
 	PRINT_D(INIT_DBG, "Freeing FW buffer ...\n");
 	PRINT_D(INIT_DBG, "Releasing firmware\n");
 	release_firmware(wilc->firmware);
+	wilc->firmware = NULL;
 
 	PRINT_D(INIT_DBG, "Download Succeeded\n");
 