IzumiYouka/android_kernel_xiaomi_sm8250

commit 56875a6a914daf15001bd7dfb64f008a4afde077

esoc: Add snapshot of esoc driver
This is a snapshot of the esoc driver as of msm-4.14 commit
<55e5efd553c10ef>. (Merge "nl80211: Fix external_auth check
for offloaded authentication")

Change-Id: Id47bcba3a5342986c3496707eb1e7cec03f19e3b
Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>

drivers/esoc/esoc-mdm-dbg-eng.c
@@ -1,0 +1,362 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2015, 2017-2018, The Linux Foundation. All rights reserved.
+ */
+#include <linux/atomic.h>
+#include <linux/device.h>
+#include "esoc.h"
+
+/*
+ * cmd_mask : Specifies if a command/notifier is masked, and
+ * whats the trigger value for mask to take effect.
+ * @mask_trigger: trigger value for mask.
+ * @mask: boolean to determine if command should be masked.
+ */
+struct esoc_mask {
+	atomic_t mask_trigger;
+	bool mask;
+};
+
+/*
+ * manual_to_esoc_cmd: Converts a user provided command
+ * to a corresponding esoc command.
+ * @cmd: ESOC command
+ * @manual_cmd: user specified command string.
+ */
+struct manual_to_esoc_cmd {
+	unsigned int cmd;
+	char manual_cmd[20];
+};
+
+/*
+ * manual_to_esoc_notify: Converts a user provided notification
+ * to corresponding esoc notification for Primary SOC.
+ * @notfication: ESOC notification.
+ * @manual_notifier: user specified notification string.
+ */
+struct manual_to_esoc_notify {
+	unsigned int notify;
+	char manual_notify[20];
+};
+
+static const struct manual_to_esoc_cmd cmd_map[] = {
+	{
+		.cmd = ESOC_PWR_ON,
+		.manual_cmd = "PON",
+	},
+	{
+		.cmd = ESOC_PREPARE_DEBUG,
+		.manual_cmd = "ENTER_DLOAD",
+	},
+	{	.cmd = ESOC_PWR_OFF,
+		.manual_cmd = "POFF",
+	},
+	{
+		.cmd = ESOC_FORCE_PWR_OFF,
+		.manual_cmd = "FORCE_POFF",
+	},
+};
+
+static struct esoc_mask cmd_mask[] = {
+	[ESOC_PWR_ON] = {
+		.mask = false,
+		.mask_trigger = ATOMIC_INIT(1),
+	},
+	[ESOC_PREPARE_DEBUG] = {
+		.mask = false,
+		.mask_trigger = ATOMIC_INIT(0),
+	},
+	[ESOC_PWR_OFF] = {
+		.mask = false,
+		.mask_trigger = ATOMIC_INIT(0),
+	},
+	[ESOC_FORCE_PWR_OFF] = {
+		.mask = false,
+		.mask_trigger = ATOMIC_INIT(0),
+	},
+};
+
+static const struct manual_to_esoc_notify notify_map[] = {
+	{
+		.notify = ESOC_PRIMARY_REBOOT,
+		.manual_notify = "REBOOT",
+	},
+	{
+		.notify = ESOC_PRIMARY_CRASH,
+		.manual_notify = "PANIC",
+	},
+};
+
+static struct esoc_mask notify_mask[] = {
+	[ESOC_PRIMARY_REBOOT] = {
+		.mask = false,
+		.mask_trigger = ATOMIC_INIT(0),
+	},
+	[ESOC_PRIMARY_CRASH] = {
+		.mask = false,
+		.mask_trigger = ATOMIC_INIT(0),
+	},
+};
+
+bool dbg_check_cmd_mask(unsigned int cmd)
+{
+	pr_debug("command to mask %d\n", cmd);
+	if (cmd_mask[cmd].mask)
+		return atomic_add_negative(-1, &cmd_mask[cmd].mask_trigger);
+	else
+		return false;
+}
+EXPORT_SYMBOL(dbg_check_cmd_mask);
+
+bool dbg_check_notify_mask(unsigned int notify)
+{
+	pr_debug("notifier to mask %d\n", notify);
+	if (notify_mask[notify].mask)
+		return atomic_add_negative(-1,
+					&notify_mask[notify].mask_trigger);
+	else
+		return false;
+}
+EXPORT_SYMBOL(dbg_check_notify_mask);
+/*
+ * Create driver attributes that let you mask
+ * specific commands.
+ */
+static ssize_t command_mask_store(struct device_driver *drv, const char *buf,
+							size_t count)
+{
+	unsigned int cmd, i;
+
+	pr_debug("user input command %s", buf);
+	for (i = 0; i < ARRAY_SIZE(cmd_map); i++) {
+		if (!strcmp(cmd_map[i].manual_cmd, buf)) {
+			/*
+			 * Map manual command string to ESOC command
+			 * set mask for ESOC command
+			 */
+			cmd = cmd_map[i].cmd;
+			cmd_mask[cmd].mask = true;
+			pr_debug("Setting mask for manual command %s\n",
+								buf);
+			break;
+		}
+	}
+	if (i >= ARRAY_SIZE(cmd_map))
+		pr_err("invalid command specified\n");
+	return count;
+}
+static DRIVER_ATTR_WO(command_mask);
+
+static ssize_t notifier_mask_store(struct device_driver *drv, const char *buf,
+							size_t count)
+{
+	unsigned int notify, i;
+
+	pr_debug("user input notifier %s", buf);
+	for (i = 0; i < ARRAY_SIZE(notify_map); i++) {
+		if (!strcmp(buf, notify_map[i].manual_notify)) {
+			/*
+			 * Map manual notifier string to primary soc
+			 * notifier. Also set mask for the notifier.
+			 */
+			notify = notify_map[i].notify;
+			notify_mask[notify].mask = true;
+			pr_debug("Setting mask for manual notification %s\n",
+									buf);
+			break;
+		}
+	}
+	if (i >= ARRAY_SIZE(notify_map))
+		pr_err("invalid notifier specified\n");
+	return count;
+}
+static DRIVER_ATTR_WO(notifier_mask);
+
+#ifdef CONFIG_MDM_DBG_REQ_ENG
+static struct esoc_clink *dbg_clink;
+/* Last recorded request from esoc */
+static enum esoc_req last_req;
+static DEFINE_SPINLOCK(req_lock);
+/*
+ * esoc_to_user: Conversion of esoc ids to user visible strings
+ * id: esoc request, command, notifier, event id
+ * str: string equivalent of the above
+ */
+struct esoc_to_user {
+	unsigned int id;
+	char str[20];
+};
+
+static struct esoc_to_user in_to_resp[] = {
+	{
+		.id = ESOC_IMG_XFER_DONE,
+		.str = "XFER_DONE",
+	},
+	{
+		.id = ESOC_BOOT_DONE,
+		.str = "BOOT_DONE",
+	},
+	{
+		.id = ESOC_BOOT_FAIL,
+		.str = "BOOT_FAIL",
+	},
+	{
+		.id = ESOC_IMG_XFER_RETRY,
+		.str = "XFER_RETRY",
+	},
+	{	.id = ESOC_IMG_XFER_FAIL,
+		.str = "XFER_FAIL",
+	},
+	{
+		.id = ESOC_UPGRADE_AVAILABLE,
+		.str = "UPGRADE",
+	},
+	{	.id = ESOC_DEBUG_DONE,
+		.str = "DEBUG_DONE",
+	},
+	{
+		.id = ESOC_DEBUG_FAIL,
+		.str = "DEBUG_FAIL",
+	},
+};
+
+static struct esoc_to_user req_to_str[] = {
+	{
+		.id = ESOC_REQ_IMG,
+		.str = "REQ_IMG",
+	},
+	{
+		.id = ESOC_REQ_DEBUG,
+		.str = "REQ_DEBUG",
+	},
+	{
+		.id = ESOC_REQ_SHUTDOWN,
+		.str = "REQ_SHUTDOWN",
+	},
+};
+
+static ssize_t req_eng_resp_store(struct device_driver *drv, const char *buf,
+							size_t count)
+{
+	unsigned int i;
+	const struct esoc_clink_ops *const clink_ops = dbg_clink->clink_ops;
+
+	dev_dbg(&dbg_clink->dev, "user input req eng response %s\n", buf);
+	for (i = 0; i < ARRAY_SIZE(in_to_resp); i++) {
+		size_t len1 = strlen(buf);
+		size_t len2 = strlen(in_to_resp[i].str);
+
+		if (len1 == len2 && !strcmp(buf, in_to_resp[i].str)) {
+			clink_ops->notify(in_to_resp[i].id, dbg_clink);
+			break;
+		}
+	}
+	if (i > ARRAY_SIZE(in_to_resp))
+		dev_err(&dbg_clink->dev, "Invalid resp %s, specified\n", buf);
+	return count;
+}
+
+static DRIVER_ATTR_WO(req_eng_resp);
+
+static ssize_t last_esoc_req_show(struct device_driver *drv, char *buf)
+{
+	unsigned int i;
+	unsigned long flags;
+	size_t count;
+
+	spin_lock_irqsave(&req_lock, flags);
+	for (i = 0; i < ARRAY_SIZE(req_to_str); i++) {
+		if (last_req == req_to_str[i].id) {
+			count = snprintf(buf, PAGE_SIZE, "%s\n",
+					req_to_str[i].str);
+			break;
+		}
+	}
+	spin_unlock_irqrestore(&req_lock, flags);
+	return count;
+}
+static DRIVER_ATTR_RO(last_esoc_req);
+
+static void esoc_handle_req(enum esoc_req req, struct esoc_eng *eng)
+{
+	unsigned long flags;
+
+	spin_lock_irqsave(&req_lock, flags);
+	last_req = req;
+	spin_unlock_irqrestore(&req_lock, flags);
+}
+
+static void esoc_handle_evt(enum esoc_evt evt, struct esoc_eng *eng)
+{
+}
+
+static struct esoc_eng dbg_req_eng = {
+	.handle_clink_req = esoc_handle_req,
+	.handle_clink_evt = esoc_handle_evt,
+};
+
+int register_dbg_req_eng(struct esoc_clink *clink,
+					struct device_driver *drv)
+{
+	int ret;
+
+	dbg_clink = clink;
+	ret = driver_create_file(drv, &driver_attr_req_eng_resp);
+	if (ret)
+		return ret;
+	ret = driver_create_file(drv, &driver_attr_last_esoc_req);
+	if (ret) {
+		dev_err(&clink->dev, "Unable to create last esoc req\n");
+		goto last_req_err;
+	}
+	ret = esoc_clink_register_req_eng(clink, &dbg_req_eng);
+	if (ret) {
+		pr_err("Unable to register req eng\n");
+		goto req_eng_fail;
+	}
+	spin_lock_init(&req_lock);
+	return 0;
+last_req_err:
+	driver_remove_file(drv, &driver_attr_last_esoc_req);
+req_eng_fail:
+	driver_remove_file(drv, &driver_attr_req_eng_resp);
+	return ret;
+}
+#else
+int register_dbg_req_eng(struct esoc_clink *clink, struct device_driver *d)
+{
+	return 0;
+}
+#endif
+
+int mdm_dbg_eng_init(struct esoc_drv *esoc_drv,
+			struct esoc_clink *clink)
+{
+	int ret;
+	struct device_driver *drv = &esoc_drv->driver;
+
+	ret = driver_create_file(drv, &driver_attr_command_mask);
+	if (ret) {
+		pr_err("Unable to create command mask file\n");
+		goto cmd_mask_err;
+	}
+	ret = driver_create_file(drv, &driver_attr_notifier_mask);
+	if (ret) {
+		pr_err("Unable to create notify mask file\n");
+		goto notify_mask_err;
+	}
+	ret = register_dbg_req_eng(clink, drv);
+	if (ret) {
+		pr_err("Failed to register esoc dbg req eng\n");
+		goto dbg_req_fail;
+	}
+	return 0;
+dbg_req_fail:
+	driver_remove_file(drv, &driver_attr_notifier_mask);
+notify_mask_err:
+	driver_remove_file(drv, &driver_attr_command_mask);
+cmd_mask_err:
+	return ret;
+}
+EXPORT_SYMBOL(mdm_dbg_eng_init);
+MODULE_LICENSE("GPL v2");

drivers/Kconfig
@@ -217,4 +217,6 @@ source "drivers/siox/Kconfig"
 
 source "drivers/slimbus/Kconfig"
 
+source "drivers/esoc/Kconfig"
+
 endmenu

drivers/Makefile
@@ -183,5 +183,6 @@ obj-$(CONFIG_FPGA)		+= fpga/
 obj-$(CONFIG_FSI)		+= fsi/
 obj-$(CONFIG_TEE)		+= tee/
 obj-$(CONFIG_MULTIPLEXER)	+= mux/
+obj-$(CONFIG_ESOC)              += esoc/
 obj-$(CONFIG_UNISYS_VISORBUS)	+= visorbus/
 obj-$(CONFIG_SIOX)		+= siox/

drivers/esoc/Kconfig
@@ -1,0 +1,73 @@
+# SPDX-License-Identifier: GPL-2.0
+#
+# External soc control infrastructure and drivers
+#
+menuconfig ESOC
+	bool "External SOCs Control"
+	help
+	  External SOCs can be powered on and monitored by user
+	  space or kernel drivers. Additionally they can be controlled
+	  to respond to control commands. This framework provides an
+	  interface to track events related to the external slave socs.
+
+if ESOC
+
+config ESOC_DEV
+	bool "ESOC userspace interface"
+	help
+	  Say yes here to enable a userspace representation of the control
+	  link. Userspace can register a request engine or a command engine
+	  for the external soc. It can receive event notifications from the
+	  control link.
+
+config ESOC_CLIENT
+	bool "ESOC client interface"
+	depends on OF
+	help
+	  Say yes here to enable client interface for external socs.
+	  Clients can specify the external soc that they are interested in
+	  by using device tree phandles. Based on this, clients can register
+	  for notifications from a specific soc.
+
+config ESOC_DEBUG
+	bool "ESOC debug support"
+	help
+	  Say yes here to enable debugging support in the ESOC framework
+	  and individual esoc drivers. The config basically adds extra
+	  logging information such that, in the event of a bug, this
+	  logging information could be helpful to trace it.
+
+config ESOC_MDM_4x
+	bool "Add support for external modems"
+	help
+	  In some qualcomm boards, an external modem such as mdm9x55 or sdx50m
+	  is connected to a primary msm. The primary soc can control/monitor
+	  the modem via gpios. The data communication with such modems can
+	  occur over PCIE or HSIC.
+
+config ESOC_MDM_DRV
+	tristate "Command engine for 4x series external modems"
+	help
+	  Provides a command engine to control the behavior of an external
+	  modems (such as mdm9x55 or sdx50m). That is, it extends the SSR
+	  framework to power-off, power-on or handle crash scenarios. It
+	  also listens for events on the external modem.
+
+config ESOC_MDM_DBG_ENG
+	tristate "debug engine for 4x series external modems"
+	depends on ESOC_MDM_DRV
+	help
+	  Mainly used as a debug interface to probe the modem against various
+	  scenarios. It basically provides a user interface to mask out certain
+	  commands sent by command engine to the external modem. It also allows
+	  masking of certain notifications being sent to the external modem.
+
+config MDM_DBG_REQ_ENG
+	tristate "manual request engine for 4x series external modems"
+	depends on ESOC_MDM_DBG_ENG
+	help
+	  Provides a user interface to handle incoming requests from
+	  the external modem. Allows for debugging of IPC mechanism
+	  between the external modem and the primary soc.
+
+endif

drivers/esoc/Makefile
@@ -1,0 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0
+# generic  external soc control support
+
+ccflags-$(CONFIG_ESOC_DEBUG)	:= -DDEBUG
+obj-$(CONFIG_ESOC)	+= esoc_bus.o
+obj-$(CONFIG_ESOC_DEV)	+= esoc_dev.o
+obj-$(CONFIG_ESOC_CLIENT)	+= esoc_client.o
+obj-$(CONFIG_ESOC_MDM_4x)	+= esoc-mdm-pon.o esoc-mdm-4x.o
+obj-$(CONFIG_ESOC_MDM_DRV)	+= esoc-mdm-drv.o
+obj-$(CONFIG_ESOC_MDM_DBG_ENG)	+= esoc-mdm-dbg-eng.o

drivers/esoc/esoc-mdm-drv.c
@@ -1,0 +1,527 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2013-2015, 2017-2018, The Linux Foundation. All rights reserved.
+ */
+
+#include <linux/delay.h>
+#include <linux/workqueue.h>
+#include <linux/reboot.h>
+#include <linux/of.h>
+#include <linux/esoc_client.h>
+#include "esoc.h"
+#include "esoc-mdm.h"
+#include "mdm-dbg.h"
+
+/* Default number of powerup trial requests per session */
+#define ESOC_DEF_PON_REQ	2
+static unsigned int n_pon_tries = ESOC_DEF_PON_REQ;
+module_param(n_pon_tries, uint, 0644);
+MODULE_PARM_DESC(n_pon_tries,
+"Number of power-on retrials allowed upon boot failure");
+
+enum esoc_boot_fail_action {
+	BOOT_FAIL_ACTION_RETRY,
+	BOOT_FAIL_ACTION_COLD_RESET,
+	BOOT_FAIL_ACTION_SHUTDOWN,
+	BOOT_FAIL_ACTION_PANIC,
+	BOOT_FAIL_ACTION_NOP,
+};
+
+static unsigned int boot_fail_action = BOOT_FAIL_ACTION_NOP;
+module_param(boot_fail_action, uint, 0644);
+MODULE_PARM_DESC(boot_fail_action,
+"Actions: 0:Retry PON; 1:Cold reset; 2:Power-down; 3:APQ Panic; 4:No action");
+
+enum esoc_pon_state {
+	PON_INIT,
+	PON_SUCCESS,
+	PON_RETRY,
+	PON_FAIL
+};
+
+enum {
+	 PWR_OFF = 0x1,
+	 PWR_ON,
+	 BOOT,
+	 RUN,
+	 CRASH,
+	 IN_DEBUG,
+	 SHUTDOWN,
+	 RESET,
+	 PEER_CRASH,
+};
+
+struct mdm_drv {
+	unsigned int mode;
+	struct esoc_eng cmd_eng;
+	struct completion pon_done;
+	struct completion req_eng_wait;
+	struct esoc_clink *esoc_clink;
+	enum esoc_pon_state pon_state;
+	struct workqueue_struct *mdm_queue;
+	struct work_struct ssr_work;
+	struct notifier_block esoc_restart;
+};
+#define to_mdm_drv(d)	container_of(d, struct mdm_drv, cmd_eng)
+
+static int esoc_msm_restart_handler(struct notifier_block *nb,
+		unsigned long action, void *data)
+{
+	struct mdm_drv *mdm_drv = container_of(nb, struct mdm_drv,
+					esoc_restart);
+	struct esoc_clink *esoc_clink = mdm_drv->esoc_clink;
+	const struct esoc_clink_ops *const clink_ops = esoc_clink->clink_ops;
+
+	if (action == SYS_RESTART) {
+		if (mdm_dbg_stall_notify(ESOC_PRIMARY_REBOOT))
+			return NOTIFY_OK;
+		esoc_mdm_log(
+			"Reboot notifier: Notifying esoc of cold reboot\n");
+		dev_dbg(&esoc_clink->dev, "Notifying esoc of cold reboot\n");
+		clink_ops->notify(ESOC_PRIMARY_REBOOT, esoc_clink);
+	}
+	return NOTIFY_OK;
+}
+static void mdm_handle_clink_evt(enum esoc_evt evt,
+					struct esoc_eng *eng)
+{
+	struct mdm_drv *mdm_drv = to_mdm_drv(eng);
+	bool unexpected_state = false;
+
+	switch (evt) {
+	case ESOC_INVALID_STATE:
+		esoc_mdm_log(
+		"ESOC_INVALID_STATE: Calling complete with state: PON_FAIL\n");
+		mdm_drv->pon_state = PON_FAIL;
+		complete(&mdm_drv->pon_done);
+		break;
+	case ESOC_RUN_STATE:
+		esoc_mdm_log(
+		"ESOC_RUN_STATE: Calling complete with state: PON_SUCCESS\n");
+		mdm_drv->pon_state = PON_SUCCESS;
+		mdm_drv->mode = RUN,
+		complete(&mdm_drv->pon_done);
+		break;
+	case ESOC_RETRY_PON_EVT:
+		esoc_mdm_log(
+		"ESOC_RETRY_PON_EVT: Calling complete with state: PON_RETRY\n");
+		mdm_drv->pon_state = PON_RETRY;
+		complete(&mdm_drv->pon_done);
+		break;
+	case ESOC_UNEXPECTED_RESET:
+		esoc_mdm_log("evt_state: ESOC_UNEXPECTED_RESET\n");
+		unexpected_state = true;
+	case ESOC_ERR_FATAL:
+		if (!unexpected_state)
+			esoc_mdm_log("evt_state: ESOC_ERR_FATAL\n");
+
+		/*
+		 * Modem can crash while we are waiting for pon_done during
+		 * a subsystem_get(). Setting mode to CRASH will prevent a
+		 * subsequent subsystem_get() from entering poweron ops. Avoid
+		 * this by seting mode to CRASH only if device was up and
+		 * running.
+		 */
+		if (mdm_drv->mode == CRASH)
+			esoc_mdm_log(
+			"Modem in crash state already. Ignoring.\n");
+		if (mdm_drv->mode != RUN)
+			esoc_mdm_log("Modem not up. Ignoring.\n");
+		if (mdm_drv->mode == CRASH || mdm_drv->mode != RUN)
+			return;
+		mdm_drv->mode = CRASH;
+		esoc_mdm_log("Starting SSR work\n");
+		queue_work(mdm_drv->mdm_queue, &mdm_drv->ssr_work);
+		break;
+	case ESOC_REQ_ENG_ON:
+		esoc_mdm_log(
+		"evt_state: ESOC_REQ_ENG_ON; Registered a req engine\n");
+		complete(&mdm_drv->req_eng_wait);
+		break;
+	default:
+		break;
+	}
+}
+
+static void mdm_ssr_fn(struct work_struct *work)
+{
+	struct mdm_drv *mdm_drv = container_of(work, struct mdm_drv, ssr_work);
+
+	/*
+	 * If restarting esoc fails, the SSR framework triggers a kernel panic
+	 */
+	esoc_clink_request_ssr(mdm_drv->esoc_clink);
+}
+
+static void esoc_client_link_power_on(struct esoc_clink *esoc_clink,
+							bool mdm_crashed)
+{
+	int i;
+	struct esoc_client_hook *client_hook;
+
+	dev_dbg(&esoc_clink->dev, "Calling power_on hooks\n");
+	esoc_mdm_log(
+	"Calling power_on hooks with crash state: %d\n", mdm_crashed);
+
+	for (i = 0; i < ESOC_MAX_HOOKS; i++) {
+		client_hook = esoc_clink->client_hook[i];
+		if (client_hook && client_hook->esoc_link_power_on)
+			client_hook->esoc_link_power_on(client_hook->priv,
+							mdm_crashed);
+	}
+}
+
+static void esoc_client_link_power_off(struct esoc_clink *esoc_clink,
+							bool mdm_crashed)
+{
+	int i;
+	struct esoc_client_hook *client_hook;
+
+	dev_dbg(&esoc_clink->dev, "Calling power_off hooks\n");
+	esoc_mdm_log(
+	"Calling power_off hooks with crash state: %d\n", mdm_crashed);
+
+	for (i = 0; i < ESOC_MAX_HOOKS; i++) {
+		client_hook = esoc_clink->client_hook[i];
+		if (client_hook && client_hook->esoc_link_power_off) {
+			client_hook->esoc_link_power_off(client_hook->priv,
+							mdm_crashed);
+		}
+	}
+}
+
+static void mdm_crash_shutdown(const struct subsys_desc *mdm_subsys)
+{
+	struct esoc_clink *esoc_clink =
+					container_of(mdm_subsys,
+							struct esoc_clink,
+								subsys);
+	const struct esoc_clink_ops * const clink_ops = esoc_clink->clink_ops;
+
+	esoc_mdm_log("MDM crashed notification from SSR\n");
+
+	if (mdm_dbg_stall_notify(ESOC_PRIMARY_CRASH))
+		return;
+	clink_ops->notify(ESOC_PRIMARY_CRASH, esoc_clink);
+}
+
+static int mdm_subsys_shutdown(const struct subsys_desc *crashed_subsys,
+							bool force_stop)
+{
+	int ret;
+	struct esoc_clink *esoc_clink =
+	 container_of(crashed_subsys, struct esoc_clink, subsys);
+	struct mdm_drv *mdm_drv = esoc_get_drv_data(esoc_clink);
+	const struct esoc_clink_ops * const clink_ops = esoc_clink->clink_ops;
+
+	esoc_mdm_log("Shutdown request from SSR\n");
+
+	if (mdm_drv->mode == CRASH || mdm_drv->mode == PEER_CRASH) {
+		esoc_mdm_log("Shutdown in crash mode\n");
+		if (mdm_dbg_stall_cmd(ESOC_PREPARE_DEBUG))
+			/* We want to mask debug command.
+			 * In this case return success
+			 * to move to next stage
+			 */
+			return 0;
+
+		esoc_clink_queue_request(ESOC_REQ_CRASH_SHUTDOWN, esoc_clink);
+		esoc_client_link_power_off(esoc_clink, true);
+
+		esoc_mdm_log("Executing the ESOC_PREPARE_DEBUG command\n");
+		ret = clink_ops->cmd_exe(ESOC_PREPARE_DEBUG,
+							esoc_clink);
+		if (ret) {
+			esoc_mdm_log("ESOC_PREPARE_DEBUG command failed\n");
+			dev_err(&esoc_clink->dev, "failed to enter debug\n");
+			return ret;
+		}
+		mdm_drv->mode = IN_DEBUG;
+	} else if (!force_stop) {
+		esoc_mdm_log("Graceful shutdown mode\n");
+		if (esoc_clink->subsys.sysmon_shutdown_ret) {
+			esoc_mdm_log(
+			"Executing the ESOC_FORCE_PWR_OFF command\n");
+			ret = clink_ops->cmd_exe(ESOC_FORCE_PWR_OFF,
+							esoc_clink);
+		} else {
+			if (mdm_dbg_stall_cmd(ESOC_PWR_OFF))
+				/* Since power off command is masked
+				 * we return success, and leave the state
+				 * of the command engine as is.
+				 */
+				return 0;
+			dev_dbg(&esoc_clink->dev, "Sending sysmon-shutdown\n");
+			esoc_mdm_log("Executing the ESOC_PWR_OFF command\n");
+			ret = clink_ops->cmd_exe(ESOC_PWR_OFF, esoc_clink);
+		}
+		if (ret) {
+			esoc_mdm_log(
+			"Executing the ESOC_PWR_OFF command failed\n");
+			dev_err(&esoc_clink->dev, "failed to exe power off\n");
+			return ret;
+		}
+		esoc_client_link_power_off(esoc_clink, false);
+		/* Pull the reset line low to turn off the device */
+		clink_ops->cmd_exe(ESOC_FORCE_PWR_OFF, esoc_clink);
+		mdm_drv->mode = PWR_OFF;
+	}
+	esoc_mdm_log("Shutdown completed\n");
+	return 0;
+}
+
+static void mdm_subsys_retry_powerup_cleanup(struct esoc_clink *esoc_clink)
+{
+	struct mdm_ctrl *mdm = get_esoc_clink_data(esoc_clink);
+	struct mdm_drv *mdm_drv = esoc_get_drv_data(esoc_clink);
+
+	esoc_mdm_log("Doing cleanup\n");
+
+	esoc_client_link_power_off(esoc_clink, false);
+	mdm_disable_irqs(mdm);
+	mdm_drv->pon_state = PON_INIT;
+	reinit_completion(&mdm_drv->pon_done);
+	reinit_completion(&mdm_drv->req_eng_wait);
+}
+
+/* Returns 0 to proceed towards another retry, or an error code to quit */
+static int mdm_handle_boot_fail(struct esoc_clink *esoc_clink, u8 *pon_trial)
+{
+	struct mdm_ctrl *mdm = get_esoc_clink_data(esoc_clink);
+
+	switch (boot_fail_action) {
+	case BOOT_FAIL_ACTION_RETRY:
+		mdm_subsys_retry_powerup_cleanup(esoc_clink);
+		esoc_mdm_log("Request to retry a warm reset\n");
+		(*pon_trial)++;
+		break;
+	/*
+	 * Issue a shutdown here and rerun the powerup again.
+	 * This way it becomes a cold reset. Else, we end up
+	 * issuing a cold reset & a warm reset back to back.
+	 */
+	case BOOT_FAIL_ACTION_COLD_RESET:
+		mdm_subsys_retry_powerup_cleanup(esoc_clink);
+		esoc_mdm_log("Doing cold reset by power-down and warm reset\n");
+		(*pon_trial)++;
+		mdm_power_down(mdm);
+		break;
+	case BOOT_FAIL_ACTION_PANIC:
+		esoc_mdm_log("Calling panic!!\n");
+		panic("Panic requested on external modem boot failure\n");
+		break;
+	case BOOT_FAIL_ACTION_NOP:
+		esoc_mdm_log("Leaving the modem in its curent state\n");
+		return -EIO;
+	case BOOT_FAIL_ACTION_SHUTDOWN:
+	default:
+		mdm_subsys_retry_powerup_cleanup(esoc_clink);
+		esoc_mdm_log("Shutdown the modem and quit\n");
+		mdm_power_down(mdm);
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int mdm_subsys_powerup(const struct subsys_desc *crashed_subsys)
+{
+	int ret;
+	struct esoc_clink *esoc_clink =
+				container_of(crashed_subsys, struct esoc_clink,
+								subsys);
+	struct mdm_drv *mdm_drv = esoc_get_drv_data(esoc_clink);
+	const struct esoc_clink_ops * const clink_ops = esoc_clink->clink_ops;
+	int timeout = INT_MAX;
+	u8 pon_trial = 1;
+
+	esoc_mdm_log("Powerup request from SSR\n");
+
+	do {
+		esoc_mdm_log("Boot trial: %d\n", pon_trial);
+		if (!esoc_clink->auto_boot &&
+			!esoc_req_eng_enabled(esoc_clink)) {
+			esoc_mdm_log("Wait for req eng registration\n");
+			dev_dbg(&esoc_clink->dev,
+					"Wait for req eng registration\n");
+			wait_for_completion(&mdm_drv->req_eng_wait);
+		}
+		esoc_mdm_log("Req eng available\n");
+		if (mdm_drv->mode == PWR_OFF) {
+			esoc_mdm_log("In normal power-on mode\n");
+			if (mdm_dbg_stall_cmd(ESOC_PWR_ON))
+				return -EBUSY;
+			esoc_mdm_log("Executing the ESOC_PWR_ON command\n");
+			ret = clink_ops->cmd_exe(ESOC_PWR_ON, esoc_clink);
+			if (ret) {
+				esoc_mdm_log("ESOC_PWR_ON command failed\n");
+				dev_err(&esoc_clink->dev, "pwr on fail\n");
+				return ret;
+			}
+			esoc_client_link_power_on(esoc_clink, false);
+		} else if (mdm_drv->mode == IN_DEBUG) {
+			esoc_mdm_log("In SSR power-on mode\n");
+			esoc_mdm_log("Executing the ESOC_EXIT_DEBUG command\n");
+			ret = clink_ops->cmd_exe(ESOC_EXIT_DEBUG, esoc_clink);
+			if (ret) {
+				esoc_mdm_log(
+				"ESOC_EXIT_DEBUG command failed\n");
+				dev_err(&esoc_clink->dev,
+						"cannot exit debug mode\n");
+				return ret;
+			}
+			mdm_drv->mode = PWR_OFF;
+			esoc_mdm_log("Executing the ESOC_PWR_ON command\n");
+			ret = clink_ops->cmd_exe(ESOC_PWR_ON, esoc_clink);
+			if (ret) {
+				dev_err(&esoc_clink->dev, "pwr on fail\n");
+				return ret;
+			}
+			esoc_client_link_power_on(esoc_clink, true);
+		}
+
+		/*
+		 * In autoboot case, it is possible that we can forever wait for
+		 * boot completion, when esoc fails to boot. This is because
+		 * there is no helper application which can alert esoc driver
+		 * about boot failure. Prevent going to wait forever in such
+		 * case.
+		 */
+		if (esoc_clink->auto_boot)
+			timeout = 10 * HZ;
+		esoc_mdm_log(
+		"Modem turned-on. Waiting for pon_done notification..\n");
+		ret = wait_for_completion_timeout(&mdm_drv->pon_done, timeout);
+		if (mdm_drv->pon_state == PON_FAIL || ret <= 0) {
+			dev_err(&esoc_clink->dev, "booting failed\n");
+			esoc_mdm_log("booting failed\n");
+			ret = mdm_handle_boot_fail(esoc_clink, &pon_trial);
+			if (ret)
+				return ret;
+		} else if (mdm_drv->pon_state == PON_RETRY) {
+			esoc_mdm_log(
+			"Boot failed. Doing cleanup and attempting to retry\n");
+			pon_trial++;
+			mdm_subsys_retry_powerup_cleanup(esoc_clink);
+		} else if (mdm_drv->pon_state == PON_SUCCESS) {
+			break;
+		}
+	} while (pon_trial <= n_pon_tries);
+
+	return 0;
+}
+
+static int mdm_subsys_ramdumps(int want_dumps,
+				const struct subsys_desc *crashed_subsys)
+{
+	int ret;
+	struct esoc_clink *esoc_clink =
+				container_of(crashed_subsys, struct esoc_clink,
+								subsys);
+	const struct esoc_clink_ops * const clink_ops = esoc_clink->clink_ops;
+
+	esoc_mdm_log("Ramdumps called from SSR\n");
+
+	if (want_dumps) {
+		esoc_mdm_log("Executing the ESOC_EXE_DEBUG command\n");
+		ret = clink_ops->cmd_exe(ESOC_EXE_DEBUG, esoc_clink);
+		if (ret) {
+			esoc_mdm_log(
+			"Failed executing the ESOC_EXE_DEBUG command\n");
+			dev_err(&esoc_clink->dev, "debugging failed\n");
+			return ret;
+		}
+	}
+	return 0;
+}
+
+static int mdm_register_ssr(struct esoc_clink *esoc_clink)
+{
+	struct subsys_desc *subsys = &esoc_clink->subsys;
+
+	subsys->shutdown = mdm_subsys_shutdown;
+	subsys->ramdump = mdm_subsys_ramdumps;
+	subsys->powerup = mdm_subsys_powerup;
+	subsys->crash_shutdown = mdm_crash_shutdown;
+	return esoc_clink_register_ssr(esoc_clink);
+}
+
+int esoc_ssr_probe(struct esoc_clink *esoc_clink, struct esoc_drv *drv)
+{
+	int ret;
+	struct mdm_drv *mdm_drv;
+	struct esoc_eng *esoc_eng;
+
+	mdm_drv = devm_kzalloc(&esoc_clink->dev, sizeof(*mdm_drv), GFP_KERNEL);
+	if (IS_ERR_OR_NULL(mdm_drv))
+		return PTR_ERR(mdm_drv);
+	esoc_eng = &mdm_drv->cmd_eng;
+	esoc_eng->handle_clink_evt = mdm_handle_clink_evt;
+	ret = esoc_clink_register_cmd_eng(esoc_clink, esoc_eng);
+	if (ret) {
+		dev_err(&esoc_clink->dev, "failed to register cmd engine\n");
+		return ret;
+	}
+	ret = mdm_register_ssr(esoc_clink);
+	if (ret)
+		goto ssr_err;
+	mdm_drv->mdm_queue = alloc_workqueue("mdm_drv_queue", 0, 0);
+	if (!mdm_drv->mdm_queue) {
+		dev_err(&esoc_clink->dev, "could not create mdm_queue\n");
+		goto queue_err;
+	}
+	esoc_set_drv_data(esoc_clink, mdm_drv);
+	init_completion(&mdm_drv->pon_done);
+	init_completion(&mdm_drv->req_eng_wait);
+	INIT_WORK(&mdm_drv->ssr_work, mdm_ssr_fn);
+	mdm_drv->esoc_clink = esoc_clink;
+	mdm_drv->mode = PWR_OFF;
+	mdm_drv->pon_state = PON_INIT;
+	mdm_drv->esoc_restart.notifier_call = esoc_msm_restart_handler;
+	ret = register_reboot_notifier(&mdm_drv->esoc_restart);
+	if (ret)
+		dev_err(&esoc_clink->dev, "register for reboot failed\n");
+	ret = mdm_dbg_eng_init(drv, esoc_clink);
+	if (ret) {
+		debug_init_done = false;
+		dev_err(&esoc_clink->dev, "dbg engine failure\n");
+	} else {
+		dev_dbg(&esoc_clink->dev, "dbg engine initialized\n");
+		debug_init_done = true;
+	}
+
+	return 0;
+queue_err:
+	esoc_clink_unregister_ssr(esoc_clink);
+ssr_err:
+	esoc_clink_unregister_cmd_eng(esoc_clink, esoc_eng);
+	return ret;
+}
+
+static struct esoc_compat compat_table[] = {
+	{
+		.name = "MDM9x55",
+		.data = NULL,
+	},
+	{
+		.name = "SDX50M",
+		.data = NULL,
+	},
+};
+
+static struct esoc_drv esoc_ssr_drv = {
+	.owner = THIS_MODULE,
+	.probe = esoc_ssr_probe,
+	.compat_table = compat_table,
+	.compat_entries = ARRAY_SIZE(compat_table),
+	.driver = {
+		.name = "mdm-4x",
+	},
+};
+
+int __init esoc_ssr_init(void)
+{
+	return esoc_drv_register(&esoc_ssr_drv);
+}
+module_init(esoc_ssr_init);
+MODULE_LICENSE("GPL v2");

commit 6cff1c2855e735d9f258fea6535f8de4e8c9f3ba

esoc: Fix a uninitalized variable warning
Fix an uninitalized variable compiler warning from last_esoc_req_show().

Fixes: 56875a6a914d ("esoc: Add snapshot of esoc driver")
Change-Id: Ic0dedbad09da5c9d1ab5d2897ec16b27966f0ebb
Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

drivers/esoc/esoc-mdm-dbg-eng.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2015, 2017-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015, 2017-2019, The Linux Foundation. All rights reserved.
  */
 #include <linux/atomic.h>
 #include <linux/device.h>
@@ -262,7 +262,7 @@ static ssize_t last_esoc_req_show(struct device_driver *drv, char *buf)
 {
 	unsigned int i;
 	unsigned long flags;
-	size_t count;
+	size_t count = 0;
 
 	spin_lock_irqsave(&req_lock, flags);
 	for (i = 0; i < ARRAY_SIZE(req_to_str); i++) {