e86187077575ff5ec7a23393f7cf319a9678ef13
Brian Norris <briannorris@chromium.org>
2025-05-14 13:13:16 -0700

    genirq: Retain disable depth for managed interrupts across CPU hotplug
    Affinity-managed interrupts can be shut down and restarted during CPU
    hotunplug/plug. Thereby the interrupt may be left in an unexpected state.
    Specifically:
    
     1. Interrupt is affine to CPU N
     2. disable_irq() -> depth is 1
     3. CPU N goes offline
     4. irq_shutdown() -> depth is set to 1 (again)
     5. CPU N goes online
     6. irq_startup() -> depth is set to 0 (BUG! driver expects that the interrupt
        		     	      	        still disabled)
     7. enable_irq() -> depth underflow / unbalanced enable_irq() warning
    
    This is only a problem for managed interrupts and CPU hotplug, all other
    cases like request()/free()/request() truly needs to reset a possibly stale
    disable depth value.
    
    Provide a startup function, which takes the disable depth into account, and
    invoked it for the managed interrupts in the CPU hotplug path.
    
    This requires to change irq_shutdown() to do a depth increment instead of
    setting it to 1, which allows to retain the disable depth, but is harmless
    for the other code paths using irq_startup(), which will still reset the
    disable depth unconditionally to keep the original correct behaviour.
    
    A kunit tests will be added separately to cover some of these aspects.
    
    [ tglx: Massaged changelog ]
    
    Suggested-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/all/20250514201353.3481400-2-briannorris@chromium.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/irq/cpuhotplug.c
+++ kernel/irq/cpuhotplug.c
@@ -241,10 +241,8 @@ static void irq_restore_affinity_of_irq(struct irq_desc *desc, unsigned int cpu)
 	    !irq_data_get_irq_chip(data) || !cpumask_test_cpu(cpu, affinity))
 		return;
 
-	if (irqd_is_managed_and_shutdown(data)) {
-		irq_startup(desc, IRQ_RESEND, IRQ_START_COND);
-		return;
-	}
+	if (irqd_is_managed_and_shutdown(data))
+		irq_startup_managed(desc);
 
 	/*
 	 * If the interrupt can only be directed to a single target
