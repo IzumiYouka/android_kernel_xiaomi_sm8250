4f9c0dc4ff51454268e34af4aa12c664c2ff8065
IzumiYouka <nymphbeeru@gmail.com>
2026-06-12 16:08:08 +0700

    kernel: fix missing dependency fallout from smoothness picks

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -540,6 +540,7 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 
 	c->lut_max_entries = i;
 	c->table[i].frequency = CPUFREQ_TABLE_END;
+	c->max_index = max_index - 1;
 
 	if (c->skip_data.skip) {
 		pr_info("%s Skip: Index[%u], Frequency[%u], Core Count %u, Final Index %u Actual Index %u Prev_Freq[%u] Prev_Index[%u] Prev_CC[%u]\n",
