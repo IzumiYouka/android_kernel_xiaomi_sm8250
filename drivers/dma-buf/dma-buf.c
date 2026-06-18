fe7a96fa04620a8fcfd2f1ebdfbe815af4cf18dd
starscroch <starscroch90@gmail.com>
2026-06-18 23:16:07 +0300

    Revert "UPSTREAM: ANDROID: Delete the DMA-BUF attachment sysfs statistics"
    This reverts commit df3a13f6e9d13866fdcdc02ea96981cd822cf8a5.
    
    Signed-off-by: munel <starscroch90@gmail.com>

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -808,6 +808,7 @@ struct dma_buf_attachment *dma_buf_attach(struct dma_buf *dmabuf,
 {
 	struct dma_buf_attachment *attach;
 	int ret;
+	unsigned int attach_uid;
 
 	if (WARN_ON(!dmabuf || !dev))
 		return ERR_PTR(-EINVAL);
@@ -828,6 +829,13 @@ struct dma_buf_attachment *dma_buf_attach(struct dma_buf *dmabuf,
 	}
 	list_add(&attach->node, &dmabuf->attachments);
 
+	attach_uid = dma_buf_update_attach_uid(dmabuf);
+	mutex_unlock(&dmabuf->lock);
+	ret = dma_buf_attach_stats_setup(attach, attach_uid);
+	if (ret)
+		goto err_sysfs;
+
+
 	return attach;
 
 err_attach:
@@ -835,6 +843,7 @@ err_attach:
 	mutex_unlock(&dmabuf->lock);
 	return ERR_PTR(ret);
 
+err_sysfs:
 	dma_buf_detach(dmabuf, attach);
 	return ERR_PTR(ret);
 }
@@ -855,6 +864,7 @@ void dma_buf_detach(struct dma_buf *dmabuf, struct dma_buf_attachment *attach)
 
 	if (attach->sgt) {
 		dmabuf->ops->unmap_dma_buf(attach, attach->sgt, attach->dir);
+		dma_buf_update_attachment_map_count(attach, -1 /* delta */);
 	}
 
 	mutex_lock(&dmabuf->lock);
@@ -863,7 +873,8 @@ void dma_buf_detach(struct dma_buf *dmabuf, struct dma_buf_attachment *attach)
 		dmabuf->ops->detach(dmabuf, attach);
 
 	mutex_unlock(&dmabuf->lock);
-	kmem_cache_free(kmem_attach_pool, attach);
+	dma_buf_attach_stats_teardown(attach);
+	kfree(attach);
 }
 EXPORT_SYMBOL_GPL(dma_buf_detach);
 
@@ -913,6 +924,9 @@ struct sg_table *dma_buf_map_attachment(struct dma_buf_attachment *attach,
 		attach->dir = direction;
 	}
 
+	if (!IS_ERR(sg_table))
+		dma_buf_update_attachment_map_count(attach, 1 /* delta */);
+
 	return sg_table;
 }
 EXPORT_SYMBOL_GPL(dma_buf_map_attachment);
@@ -940,6 +954,8 @@ void dma_buf_unmap_attachment(struct dma_buf_attachment *attach,
 		return;
 
 	attach->dmabuf->ops->unmap_dma_buf(attach, sg_table, direction);
+
+	dma_buf_update_attachment_map_count(attach, -1 /* delta */);
 }
 EXPORT_SYMBOL_GPL(dma_buf_unmap_attachment);
 
