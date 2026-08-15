cc782ddb1512abf307dd4b26f413dc0285a0d9f1
IzumiYouka <nymphbeeru@gmail.com>
2026-08-15 17:51:56 +0700

    Revert "mm: Omit RCU read lock in list_lru_count_one() when RCU isn't needed"
    This reverts commit 853c542a653b489803dba2084b59721d1ed69e7e.

--- mm/list_lru.c
+++ mm/list_lru.c
@@ -183,7 +183,6 @@ EXPORT_SYMBOL_GPL(list_lru_isolate_move);
 unsigned long list_lru_count_one(struct list_lru *lru,
 				 int nid, struct mem_cgroup *memcg)
 {
-#if defined(CONFIG_MEMCG) && !defined(CONFIG_SLOB)
 	struct list_lru_node *nlru = &lru->node[nid];
 	struct list_lru_one *l;
 	unsigned long count;
@@ -194,9 +193,6 @@ unsigned long list_lru_count_one(struct list_lru *lru,
 	rcu_read_unlock();
 
 	return count;
-#else
-	return READ_ONCE(lru->node[nid].lru.nr_items);
-#endif
 }
 EXPORT_SYMBOL_GPL(list_lru_count_one);
 
